5ca8809bf2d3834d40e45335c9c4d16f3a56064d
LogixKitten <aliceariaallen@gmail.com>
2024-10-22 19:24:21 -0500

    Added server_metadata_url to oauth register.

--- app.py
+++ app.py
@@ -33,10 +33,10 @@ google = oauth.register(
     redirect_uri='https://www.jillai.tech/callback',  # Update with your redirect URL
     scope=['openid', 'email', 'profile', 'https://www.googleapis.com/auth/calendar'],
     userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # Correct user info endpoint
+    server_metadata_url= 'https://accounts.google.com/.well-known/openid-configuration'
     client_kwargs={
         'scope': 'openid email profile https://www.googleapis.com/auth/calendar',
         'token_endpoint_auth_method': 'client_secret_post',
-        'jwks_uri': 'https://www.googleapis.com/oauth2/v3/certs'  # Add this line
     }
 )
 
